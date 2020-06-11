0da72052e9170c0b6a1374dcba2cebca92131bb8
frenkie li <FrenkieFull@gmail.com>
2020-06-11 13:30:08 +0800

    feat: 加入數值判斷
    將數值作判斷後顯示在畫面上

--- src/components/mapSystemInfoItem.vue
+++ src/components/mapSystemInfoItem.vue
@@ -6,10 +6,7 @@
   */
 -->
 <template>
-  <div
-    class="map_systemInfo"
-    id="map_systemmenInfo_0"
-  >
+  <div class="map_systemInfo">
     <div class="map_systemInfo_img">
       <img :src="'./images/dashboard/' + imageSrc +'.png'" draggable="false" />
     </div>
@@ -17,30 +14,38 @@
       <div class="map_systemInfo_info_temperature map_systemInfo_info_item">
         <span>溫度</span>
         <span>
-          <span id="map_systemInfo_info_men_temperature">{{ data.property.temperature ? data.property.temperature.value : '--' }}</span>°C
+          <span>{{ temperature }}</span>
         </span>
       </div>
       <div class="map_systemInfo_info_humidity map_systemInfo_info_item">
         <span>濕度</span>
         <span>
-          <span id="map_systemInfo_info_men_humidity">{{ data.property.humidity ? data.property.humidity.value : '--' }}</span>%
+          <span>{{ humidity }}</span>
         </span>
       </div>
       <div class="map_systemInfo_info_PM25 map_systemInfo_info_item">
         <span>PM2.5</span>
-        <span id="map_systemInfo_info_men_PM25">{{ data.property.PM25 ? data.property.PM25.value : '--' }}</span>
+        <span :style="{
+          color: PM25.color,
+        }">{{ PM25.str }}</span>
       </div>
       <div class="map_systemInfo_info_CO2 map_systemInfo_info_item">
         <span>二氧化碳</span>
-        <span id="map_systemInfo_info_men_CO2">{{ data.property.CO2 ? data.property.CO2.value : '--' }}</span>
+        <span :style="{
+          color: CO2.color,
+        }">{{ CO2.str }}</span>
       </div>
       <div class="map_systemInfo_info_NH3 map_systemInfo_info_item">
         <span>氨氣</span>
-        <span id="map_systemInfo_info_men_NH3">{{ data.property.NH3 ? data.property.NH3.value : '--' }}</span>
+        <span :style="{
+          color: NH3.color,
+        }">{{ NH3.str }}</span>
       </div>
       <div class="map_systemInfo_info_H2S map_systemInfo_info_item">
         <span>硫化氫</span>
-        <span id="map_systemInfo_info_men_H2S">{{ data.property.H2S ? data.property.H2S.value : '--' }}</span>
+        <span :style="{
+          color: H2S.color,
+        }">{{ H2S.str }}</span>
       </div>
     </div>
   </div>
@@ -49,12 +54,89 @@
 <script>
 export default {
   name: "maptoiletItem",
+  data() {
+    return {
+      PM25Condition: {
+        good: 0,
+        soso: 76,
+        bad: 151
+      },
+      CO2Condition: {
+        good: 0,
+        soso: 0.15,
+        bad: 0.25
+      },
+      NH3Condition: {
+        good: 0,
+        soso: 5,
+        bad: 10
+      },
+      H2SCondition: {
+        good: 0,
+        soso: 5,
+        bad: 10
+      }
+    };
+  },
   props: {
     data: Object
   },
   computed: {
-    imageSrc : function(){
-      return this.data._id.replace('map_system', '').split('Info')[0]
+    imageSrc: function() {
+      return this.data._id.replace("map_system", "").split("Info")[0];
+    },
+    temperature: function() {
+      let vm = this;
+      return vm.data.property.temperature
+        ? vm.data.property.temperature.value + "°C"
+        : "--";
+    },
+    humidity: function() {
+      let vm = this;
+      return vm.data.property.humidity
+        ? vm.data.property.humidity.value / 100 + "%"
+        : "--";
+    },
+    PM25: function() {
+      let vm = this;
+      return vm.data.property.PM25 ? vm.checkQuality(vm.PM25Condition, vm.data.property.PM25.value) : {str: "--"};
+    },
+    CO2: function() {
+      let vm = this;
+      return vm.data.property.CO2 ? vm.checkQuality(vm.CO2Condition, vm.data.property.CO2.value) : {str: "--"};
+    },
+    NH3: function() {
+      let vm = this;
+      return vm.data.property.NH3 ? vm.checkQuality(vm.NH3Condition, vm.data.property.NH3.value) : {str: "--"};
+    },
+    H2S: function() {
+      let vm = this;
+      return vm.data.property.H2S ? vm.checkQuality(vm.H2SCondition, vm.data.property.H2S.value) : {str: "--"};
+    }
+  },
+  methods: {
+    checkQuality(condition, value) {
+      let data = {
+        str: "",
+        color: "#000"
+      };
+      if (value >= condition.bad) {
+        data = {
+          str: "汙染",
+          color: "#f00"
+        };
+      } else if (value >= condition.soso) {
+        data = {
+          str: "良",
+          color: "#ff0"
+        };
+      } else {
+        data = {
+          str: "優秀",
+          color: "#0f0"
+        };
+      }
+      return data;
     }
   }
 };
@@ -64,16 +146,16 @@ export default {
 div.map_systemInfo {
   display: flex;
   align-items: center;
-  .map_systemInfo_img{
+  .map_systemInfo_img {
     width: 50px;
-    img{
+    img {
       vertical-align: middle;
       width: 100%;
     }
   }
   .map_systemInfo_info {
     display: flex;
-    .map_systemInfo_info_item{
+    .map_systemInfo_info_item {
       min-width: 70px;
       margin: 5px;
       display: flex;
